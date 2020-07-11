e749daed6b046f3f17516ca8292cbd87a0b67f6c
NgnDien <NghiaAndeveloper@outlook.com>
2020-07-11 16:19:56 +0700

    Add project files.

---
+++ SlideShareDownloader/FrmMain.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace SlideShareDownloader
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLink = new System.Windows.Forms.TextBox();
+            this.txtDir = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnDir = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Link tài liệu:";
+            // 
+            // txtLink
+            // 
+            this.txtLink.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLink.Location = new System.Drawing.Point(110, 10);
+            this.txtLink.Margin = new System.Windows.Forms.Padding(4);
+            this.txtLink.Name = "txtLink";
+            this.txtLink.Size = new System.Drawing.Size(461, 26);
+            this.txtLink.TabIndex = 1;
+            // 
+            // txtDir
+            // 
+            this.txtDir.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDir.Location = new System.Drawing.Point(110, 48);
+            this.txtDir.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDir.Name = "txtDir";
+            this.txtDir.Size = new System.Drawing.Size(423, 26);
+            this.txtDir.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(13, 51);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Thư mục lưu:";
+            // 
+            // btnDir
+            // 
+            this.btnDir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDir.Location = new System.Drawing.Point(537, 47);
+            this.btnDir.Margin = new System.Windows.Forms.Padding(0);
+            this.btnDir.Name = "btnDir";
+            this.btnDir.Size = new System.Drawing.Size(34, 28);
+            this.btnDir.TabIndex = 4;
+            this.btnDir.Text = "...";
+            this.btnDir.UseVisualStyleBackColor = true;
+            this.btnDir.Click += new System.EventHandler(this.btnDir_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.AutoSize = true;
+            this.btnStart.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnStart.Location = new System.Drawing.Point(475, 85);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(96, 34);
+            this.btnStart.TabIndex = 5;
+            this.btnStart.Text = "&Tải xuống";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.Description = "Chọn thư mục lưu";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Location = new System.Drawing.Point(13, 85);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(91, 34);
+            this.lblStatus.TabIndex = 6;
+            this.lblStatus.Text = "Đã tải 0/0";
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(110, 89);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(358, 23);
+            this.progressBar.TabIndex = 7;
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.WorkerReportsProgress = true;
+            this.backgroundWorker.WorkerSupportsCancellation = true;
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_ProgressChanged);
+            this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker_RunWorkerCompleted);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 132);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnDir);
+            this.Controls.Add(this.txtDir);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtLink);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "FrmMain";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SlideShareImageDownloader - Created by Ngn";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLink;
+        private System.Windows.Forms.TextBox txtDir;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDir;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+    }
+}
+
---
+++ SlideShareDownloader/FrmMain.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace SlideShareDownloader
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDir_Click(object sender, EventArgs e)
+        {
+            if(folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtDir.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            ///còn cái kiểm tra thư mục có tồn tại hay không thì nên kiểm tra ngay trước khi lưu file nhỉ
+            //thêm cái nữa là tạo 1 cái progressbar để hiện tiến trình tải cái, đơn vị tính bằng số lượng ảnh lấy được
+            //1. kiểm tra có phải link của slideshare.net hay không; 
+            if(!CheckLink())
+            {
+                txtLink.SelectAll();
+                return;
+            }
+            //2. kiểm tra thư mục lưu
+            if(txtDir.Text == string.Empty)
+            {
+                if(MessageBox.Show("Xuất ra thư mục hiện tại?", "Xác nhận", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+                {
+                    txtDir.Focus();
+                    return;
+                }
+            }
+            //3. Tải mã nguồn trang này về, Đọc mã nguồn và tách lấy địa chỉ của các ảnh
+            if(!GetLinks(GetSrc()))
+            {
+                Err("Không tải được dữ liệu");
+                return;
+            }
+            //4. Lấy link từng ảnh và tải xuống thư mục kia
+            ProgressValue = 0;
+            ProgressMax = Links.Count;
+            Status = $"{ProgressValue}/{ProgressMax}";
+            backgroundWorker.RunWorkerAsync();
+        }
+
+        MatchCollection Links;
+        Regex tmpRex;
+
+        private bool CheckLink()
+        {
+            if(txtLink.Text == "")
+            {
+                Err("Link tài liệu không được bỏ trống");
+                return false;
+            }
+            tmpRex = new Regex("(https://){0,1}([a-zA-Z]*.){0,1}slideshare.net/[a-zA-Z0-9-./]+");
+            if(!tmpRex.IsMatch(txtLink.Text))
+            {
+                Err("Link tài liệu không đúng");
+                return false;
+            }
+            tmpRex = new Regex("slideshare.net/[a-zA-Z0-9-./]+");
+            txtLink.Text = "https://www." + tmpRex.Match(txtLink.Text).Value;
+            return true;
+        }
+
+        WebClient webClient = new WebClient();
+
+        private string GetSrc()
+        {
+            try
+            {
+                byte[] data = webClient.DownloadData(txtLink.Text);
+                if(data is null)
+                {
+                    Err("Không thể tải dữ liệu");
+                    return "";
+                }
+                return Encoding.UTF8.GetString(data);
+            }
+            catch
+            {
+                Err("Không thể tải dữ liệu");
+                return "";
+            }
+        }
+
+        private bool GetLinks(string data)
+        {
+            if(string.IsNullOrEmpty(data))
+                return false;
+            tmpRex = new Regex("data-full=\"https://[a-zA-Z0-9./-]*.jpg");
+            Links = tmpRex.Matches(data);
+            if(Links?.Count > 0)
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+        Regex rexLnk = new Regex("https://[a-zA-Z0-9./-]*.jpg");
+
+        private string GetLink(Match data)
+        {
+            return rexLnk.Match(data.Value).Value;
+        }
+
+        
+
+        Regex rexName = new Regex("[1-9][0-9]*-[1-9][0-9]{2,3}.jpg$");
+
+        private string GetName(string link)
+        {
+            return rexName.Match(link).Value;
+        }
+
+        private bool CheckDir()
+        {
+            if(!txtDir.Text.EndsWith("\\"))
+            {
+                if(InvokeRequired)
+                {
+                    Invoke((MethodInvoker)delegate
+                    {
+                        txtDir.AppendText("\\");
+                    });
+                }
+                else
+                {
+                    txtDir.AppendText("\\");
+                }
+                
+            }
+            if(Directory.Exists(txtDir.Text))
+            {
+                return true;
+            }
+            try
+            {
+                Directory.CreateDirectory(txtDir.Text);
+                return true;
+            }
+            catch
+            {
+                Err("Không tạo được thư mục lưu ảnh");
+                txtDir.SelectAll();
+                return false;
+            }
+        }
+
+        string Status
+        {
+            get
+            {
+                return lblStatus.Text;
+            }
+            set
+            {
+                if(InvokeRequired)
+                {
+                    Invoke((MethodInvoker)delegate
+                    {
+                        lblStatus.Text = value;
+                    });
+                }
+                else
+                {
+                    lblStatus.Text = value;
+                }
+            }
+        }
+
+        int ProgressMax
+        {
+            get
+            {
+                return progressBar.Maximum;
+            }
+            set
+            {
+                if(InvokeRequired)
+                {
+                    Invoke((MethodInvoker)delegate
+                    {
+                        progressBar.Maximum = value;
+                    });
+                }
+                else
+                {
+                    progressBar.Maximum = value;
+                }
+            }
+        }
+
+        int ProgressValue
+        {
+            get
+            {
+                return progressBar.Value;
+            }
+            set
+            {
+                if(InvokeRequired)
+                {
+                    Invoke((MethodInvoker)delegate
+                    {
+                        progressBar.Value = value;
+                    });
+                }
+                else
+                {
+                    progressBar.Value = value;
+                }
+            }
+        }
+
+        private DialogResult Err(string msg)
+        {
+            return MessageBox.Show(msg, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        #region Tải ảnh       
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string link = "";
+            if(!CheckDir())
+            {
+                //e.Cancel = true;
+                return;
+            }    
+            foreach(Match match in Links)
+            {
+                try
+                {
+                    webClient.DownloadFile(new Uri(link = GetLink(match)), txtDir.Text + GetName(link));
+                    ProgressValue++;
+                    Status = $"{ProgressValue}/{ProgressMax}";
+                }
+                catch
+                {
+                    Err("Tải file không thành công");
+
+                    Status = "Tải không thành công";
+                    return;
+                    //e.Cancel = true;
+                }
+                //if(backgroundWorker.CancellationPending)
+                //{
+                //    return;
+                //}
+            }    
+        }
+
+        private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if(ProgressValue == ProgressMax)
+            {
+                MessageBox.Show(Status = "Tải xong", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show($"Đã tải {ProgressValue}/{ProgressMax}", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        #endregion Tải ảnh;
+    }
+}
